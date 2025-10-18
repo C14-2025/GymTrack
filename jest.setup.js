a6a567b383caed6072da8c61f61935cb2bbb4328
KBritoR <161668608+KBritoR@users.noreply.github.com>
2025-10-17 21:45:00 -0300

    Refactor Jest setup for Next.js mocks
    Refactor Jest setup to conditionally define global Request, NextRequest, and Response mocks using Next.js specifications.

--- jest.setup.js
+++ jest.setup.js
@@ -1,28 +1,29 @@
-// Optional: configure or set up a testing framework before each test.
-// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+import "@testing-library/jest-dom" 
 
-// Used for __tests__/testing-library.js
-// Learn more: https://github.com/testing-library/jest-dom
-import "@testing-library/jest-dom"
 
-if (typeof Request === 'undefined') {
-  global.Request = require('next/dist/server/web/spec-extension/request').Request;
+try {
+
+  const NextWeb = require('next/dist/server/web/spec-extension/request');
+
+  if (typeof Request === 'undefined') {
+    global.Request = NextWeb.Request;
+  }
+  if (typeof NextRequest === 'undefined') {
+    global.NextRequest = NextWeb.NextRequest;
+  }
+
+  const ResponseWeb = require('next/dist/server/web/spec-extension/response');
+  if (typeof Response === 'undefined') {
+    global.Response = ResponseWeb.Response;
+  }
+} catch (e) {
+  console.warn("Falha ao configurar mocks de Request/Response do Next.js. Certifique-se de usar 'NextRequest' em seus testes de API.");
 }
-if (typeof NextRequest === 'undefined') {
-  global.NextRequest = require('next/dist/server/web/spec-extension/request').NextRequest;
-}
 
-// Resposta da API (Response)
-if (typeof Response === 'undefined') {
-  global.Response = require('next/dist/server/web/spec-extension/response').Response;
-}
-
-// Objeto URL, necessário para o NextRequest
 if (typeof URL === 'undefined') {
     global.URL = require('url').URL;
 }
 
-// Se o seu código usar fetch (ou o Next.js o importar), defina um mock
 if (typeof fetch === 'undefined') {
     global.fetch = jest.fn();
 }
