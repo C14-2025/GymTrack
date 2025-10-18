a6dc0364f7ea8679530284c062d736c5f78aac74
KBritoR <161668608+KBritoR@users.noreply.github.com>
2025-10-17 21:39:50 -0300

    Setup global objects for testing in jest.setup.js
    Added global polyfills and mocks for testing environment.

--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,25 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom"
+
+if (typeof Request === 'undefined') {
+  global.Request = require('next/dist/server/web/spec-extension/request').Request;
+}
+if (typeof NextRequest === 'undefined') {
+  global.NextRequest = require('next/dist/server/web/spec-extension/request').NextRequest;
+}
+
+// Resposta da API (Response)
+if (typeof Response === 'undefined') {
+  global.Response = require('next/dist/server/web/spec-extension/response').Response;
+}
+
+// Objeto URL, necessário para o NextRequest
+if (typeof URL === 'undefined') {
+    global.URL = require('url').URL;
+}
+
+// Se o seu código usar fetch (ou o Next.js o importar), defina um mock
+if (typeof fetch === 'undefined') {
+    global.fetch = jest.fn();
+}
